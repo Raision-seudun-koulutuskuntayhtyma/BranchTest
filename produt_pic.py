fa3467d2d92fd5becbb400b1837a126579940129
Mika Vainio <koti.mika.vainio@gmail.com>
2022-03-25 15:10:41 +0200

    Muokattu kuvakaappauskoodia

--- produt_pic.py
+++ produt_pic.py
@@ -6,17 +6,25 @@
 # OpenCV for image and video manipulatation
 import cv2
 
-# NumPy for array and matrice operations
+# NumPy for array and matrix operations
 import numpy
 
-# Dimensions for the video window and the still image
-picture_width = 400
-picture_height = 300
+# Ask the file name for product image
+file_name = input('Anna kuvatidoston nimi: ')
+
+# Ask the scalling ratio for product image, 1 when no scale given or invalid scale factor
+ratio_str = input('Anna kuvan skaalauskerroin: ')
+try:
+  ratio = float(ratio_str)
+except:
+  print('Invalid scale factor, using original video size')
+  ratio = 1
+
 
 # Creating a VideoCapture object for the wideo stream from the first Web camera ie unit 0
 video_stream = cv2.VideoCapture(0)
  
-# Loop until iterruptd by keystroke s -> save
+# Loop until iterrupted by keystroke s -> save
 while (video_stream.isOpened()):
 
     # Capture the stream frame by frame, read() fuction returns true if reading is successfull and a wideo frame
@@ -24,27 +32,40 @@ while (video_stream.isOpened()):
 
     # Check if capture is successfull
     if ret == True:
-        
-        # Resize the frame for database use using cubic interpolation
+
+        # Find out frame dimensions and number of channels (for proportional resizing)
+        org_height, org_width, channels = frame.shape
+
+        # Dimensions for the video window and the still image
+        picture_width = int(round(org_width * ratio, 0))
+        picture_height = int(round(org_height * ratio, 0))
+
+        # Resize the frame for database use using bicubic interpolation
         frame = cv2.resize(frame, (picture_width, picture_height), fx = 0, fy = 0, interpolation = cv2.INTER_CUBIC)
+
+        # Draw a safe area rectange into frame
+        top_left = (20, 20)
  
-    # Display the video frame
-    cv2.imshow('Video stream', frame)
+        # Display the video frame in a window named Video stream
+        cv2.imshow('Video stream', frame)
      
-    # Define key stroke s as the save and exit from video streaming 
+    # Define a key stroke s as the save and exit from video streaming, wait 25 ms for key storke
     if cv2.waitKey(25) & 0xFF == ord('s'):
         break
 
 # Save the last captured frame as jpg
-cv2.imwrite('product.jpg', frame)
-print('Kuva tallennetu työhakemistoon nimellä product.jpg')
+cv2.imwrite(file_name, frame)
+print('Kuva tallennetu työhakemistoon nimellä', file_name)
 
 # Release the video capture object
 video_stream.release()
 
 # Read the still image and show it in a new window
-still_image = cv2.imread('product.jpg')
+still_image = cv2.imread(file_name)
 cv2.imshow('Still image', still_image)
+
+# Show original frame dimensions on the console
+print('Alkuperäinen kuvakoko oli', org_width, 'x', org_height, 'pikseliä ja kuvassa on', channels, 'kanavaa')
 
 # Wait for key stroke q to close all open video and picture windows
 while True:
